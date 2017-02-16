b783ee2417241090ebaf1cca4e48cb1180202f60
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-02-16 09:14:22 +0700

    Config webpack for React

--- client/index.js
+++ client/index.js
@@ -1,5 +1,21 @@
 import _ from 'lodash'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-export default function App() {
-  return _.kebabCase('Hello World')
+export default class App extends Component {
+
+  static getNumber() {
+    return ' 1'
+  }
+
+  getHello() {
+    debugger
+    return 'HELLO WORLD' + App.getNumber()
+  }
+
+  render() {
+    return <span>{_.kebabCase(this.getHello())}</span>
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById('main'))
