ea01da81cf1977a7c908d782eb67329a009572fe
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-02-16 09:14:22 +0700

    Config webpack for React

--- dev.config.js
+++ dev.config.js
@@ -1,10 +1,41 @@
-const path = require('path')
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry  : path.resolve(__dirname, 'client'),
-  output : {
-    filename: 'index.js',
-    path    : path.resolve(__dirname, 'dist')
+  entry    : resolve(__dirname, 'client'),
+  output   : {
+    filename  : '[name].js',
+    path      : resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
-  devtool: 'cheap-module-source-map'
+  // context: resolve(__dirname, 'client'),
+  devtool  : 'inline-source-map',
+  devServer: {
+    // hot: true,
+    // enable HMR on the server
+
+    contentBase: resolve(__dirname, 'dist'),
+    // match the output path
+
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+  module   : {
+    rules: [
+      {
+        test   : /\.jsx?$/,
+        use    : ['babel-loader'],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.pug$/,
+        use : ['pug-loader']
+      }
+    ]
+  },
+  plugins  : [
+    new HtmlWebpackPlugin({
+      template: 'views/index.pug'
+    })
+  ]
 }
