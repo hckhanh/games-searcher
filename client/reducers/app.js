50878e8c139cafe69d625f195d93db9b452c3d19
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-02-26 02:17:08 +0700

    Fig bug when search with unicode characters

--- client/reducers/app.js
+++ client/reducers/app.js
@@ -9,6 +9,8 @@ export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'GET_SUGGESTIONS_SUCCESS':
       return state.merge({ suggestions: action.suggestions })
+    case 'CLEAR_SUGGESTIONS':
+      return state.merge({ suggestions: List() })
     case 'LOAD_APP':
       return state.merge({ loading: true })
     case 'LOAD_APP_DONE':
--- client/reducers/home.js
+++ client/reducers/home.js
@@ -9,6 +9,8 @@ export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'GET_GAMES_SUCCESS':
       return state.merge({ games: action.games, hasPrices: false })
+    case 'CLEAR_GAMES':
+      return state.merge({ games: List() })
     case 'GET_PRICES_SUCCESS':
       const prices = Immutable.fromJS(action.prices)
       let games = state.get('games')
