bf93bda2f68528dacf9c99a456f7e1c47f39e0d0
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-03-01 11:43:00 +0700

    Update style for top menus

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,4 +1,4 @@
-import { Button, Col, Icon, Layout, Menu, Row, Select } from 'antd'
+import { Button, Icon, Layout, Menu, Select } from 'antd'
 import React, { Component } from 'react'
 import Helmet from 'react-helmet/es/Helmet'
 import { connect } from 'react-redux'
@@ -44,86 +44,76 @@ export default class App extends Component {
     }
   }
 
-  generateSubHeader = (currency) => {
-    return (
-      <Header className='sub-header'>
-        <div className='header-content'>
-          <Select
-            showSearch
-            className='currency-dropdown'
-            size='small'
-            dropdownMatchSelectWidth={false}
-            value={currency}
-            onSelect={this.props.setCurrency}
-            filterOption={(currency, option) => option.props.value.toLowerCase().indexOf(currency.toLowerCase()) >= 0}
-          >
-            {
-              this.props.currency.getIn(['exchangeRates', 'rates'])
-                  .keySeq()
-                  .map(rate => <Option key={rate} value={rate}>{rate}</Option>)
-            }
-          </Select>
-        </div>
-      </Header>
-    )
-  }
-
-  generateMainHeader = () => {
+  generateMainHeader = (currency) => {
     return (
       <Header className='main-header'>
         <div className='header-content'>
-          <Row>
-            <Col span={5}>
-              <Link to='/'>
-                <div className='logo-icon' />
-                <div className='logo-title'>{process.env.APP_TITLE}</div>
-              </Link>
-            </Col>
-            <Col span={14}>
-              <Select
-                className='game-search'
-                combobox
-                showSearch
-                allowClear
-                size='large'
-                placeholder='Find a game'
-                notFoundContent='No result'
-                dropdownMatchSelectWidth={false}
-                showArrow={false}
-                filterOption={false}
-                onSearch={this.handleOnSuggestGames}
-                onSelect={this.handleOnSearchGames}
-              >
-                {this.generateGameData()}
-              </Select>
-              <Button className='search-btn'
-                      size='large'
-                      type='primary'
-                      onClick={() => this.handleOnSearchGames(this.state.currentName)}>
-                <Icon type='search' />
-              </Button>
-            </Col>
-            <Col span={5}>
-              <Menu
-                className='top-menu-right'
-                theme='dark'
-                mode='horizontal'
-              >
-                <Menu.Item key='1'>
-                  <a href='https://paypal.me/hckhanh/5' target='_blank'>
-                    <Icon type="like" />
-                    Donate
-                  </a>
-                </Menu.Item>
-                <Menu.Item key='2'>
-                  <a href='https://github.com/hckhanh/games-searcher' target='_blank'>
-                    <Icon type="github" />
-                    GitHub
-                  </a>
-                </Menu.Item>
-              </Menu>
-            </Col>
-          </Row>
+          <Link className='logo' to='/'>
+            <div className='logo-icon' />
+            <div className='logo-title'>{process.env.APP_TITLE}</div>
+          </Link>
+          <div className='search'>
+            <Select
+              className='game-search'
+              combobox
+              showSearch
+              allowClear
+              size='large'
+              placeholder='Find a game'
+              notFoundContent='No result'
+              dropdownMatchSelectWidth={false}
+              showArrow={false}
+              filterOption={false}
+              onSearch={this.handleOnSuggestGames}
+              onSelect={this.handleOnSearchGames}
+            >
+              {this.generateGameData()}
+            </Select>
+            <Button
+              className='search-btn'
+              size='large'
+              type='primary'
+              onClick={() => this.handleOnSearchGames(this.state.currentName)}
+            >
+              <Icon type='search' />
+            </Button>
+          </div>
+          <div className='right-items'>
+            <Menu
+              className='top-menu-right'
+              theme='dark'
+              mode='horizontal'
+            >
+              <Menu.Item key='1'>
+                <a href='https://paypal.me/hckhanh/5' target='_blank'>
+                  <Icon type="like" />
+                  Donate
+                </a>
+              </Menu.Item>
+              <Menu.Item key='2'>
+                <a href='https://github.com/hckhanh/games-searcher' target='_blank'>
+                  <Icon type="github" />
+                  GitHub
+                </a>
+              </Menu.Item>
+            </Menu>
+            <Select
+              showSearch
+              className='currency-dropdown'
+              size='small'
+              dropdownMatchSelectWidth={false}
+              value={currency}
+              onSelect={this.props.setCurrency}
+              filterOption={(currency, option) => option.props.value.toLowerCase().indexOf(currency.toLowerCase()) >= 0}
+            >
+            {
+              this
+                .props.currency.getIn(['exchangeRates', 'rates'])
+                .keySeq()
+                .map(rate => <Option key={rate} value={rate}>{rate}</Option>)
+            }
+          </Select>
+          </div>
         </div>
       </Header>
     )
@@ -168,15 +158,14 @@ export default class App extends Component {
           title={process.env.APP_TITLE}
           meta={this.generateMetaData()}
         />
-        {this.generateSubHeader(currency)}
-        {this.generateMainHeader()}
+        {this.generateMainHeader(currency)}
         <Content>
           <div className='main-content'>
             {this.props.children}
           </div>
         </Content>
         <Footer className='main-footer'>
-          Made with <Icon type="heart" /> by <a href='https://github.com/hckhanh'>@hckhanh</a>
+          Made with <Icon type="heart" /> by <a href='https://github.com/hckhanh' target='_blank'>@hckhanh</a>
         </Footer>
       </Layout>
     )
