2b8fde1315485d0c44a517eebe27d6cd300f825e
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-03-04 22:42:59 +0700

    Update suites for test cases and test command

--- tests/apis/index.test.js
+++ tests/apis/index.test.js
@@ -1,10 +1,12 @@
 const app = require('../../app')
 const request = require('supertest')
 
-test('GET / - respond index page', () => {
-  return request(app)
-    .get('/')
-    .then(res => {
-      expect(res.headers['content-type']).toEqual(expect.stringMatching(/html/))
-    })
+describe('GET /', () => {
+  test('respond index page', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.headers['content-type']).toEqual(expect.stringMatching(/html/))
+      })
+  })
 })
--- tests/apis/rates.test.js
+++ tests/apis/rates.test.js
@@ -1,15 +1,17 @@
 const app = require('../../app')
 const request = require('supertest')
 
-test('GET /rates - respond exchange rates from openexchangerates.org server', () => {
-  return request(app)
-    .get('/api/rates')
-    .then(res => {
-      expect(res.body).toEqual(
-        expect.objectContaining({
-          base : expect.any(String),
-          rates: expect.any(Object)
-        })
-      )
-    })
+describe('GET /rates', () => {
+  test('respond exchange rates from openexchangerates.org server', () => {
+    return request(app)
+      .get('/api/rates')
+      .then(res => {
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            base : expect.any(String),
+            rates: expect.any(Object)
+          })
+        )
+      })
+  })
 })
