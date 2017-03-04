38c9717516c458f0de1a1ed30c9ab327c976cee1
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-03-04 18:01:39 +0700

    Fix bug when the recommendations list show while loading

--- client/actions/app.js
+++ client/actions/app.js
@@ -1,15 +1,19 @@
 import apis from '../apis'
 
 export function getSuggestions(name) {
-  return dispatch => {
-    const url = apis.GET_SUGGESTIONS(encodeURIComponent(name))
-    fetch(url)
-      .then(res => res.json())
-      .then(games => {
-        dispatch({ type: 'GET_SUGGESTIONS_SUCCESS', suggestions: games })
-      })
-      .catch(error => {
-        dispatch({ type: 'FETCH_ERROR', error, url })
-      })
+  return (dispatch, getState) => {
+    const state = getState()
+
+    if (!state.app.get('loading')) {
+      const url = apis.GET_SUGGESTIONS(encodeURIComponent(name))
+      fetch(url)
+        .then(res => res.json())
+        .then(games => {
+          dispatch({ type: 'GET_SUGGESTIONS_SUCCESS', suggestions: games })
+        })
+        .catch(error => {
+          dispatch({ type: 'FETCH_ERROR', error, url })
+        })
+    }
   }
 }
