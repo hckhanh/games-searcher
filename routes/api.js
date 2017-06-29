3163411634a9c5701a067ceb70956bc45359a2d0
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-06-29 15:47:29 +0700

    Redude lodash package size and remove users api (unused)

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const users = require('./users')
 const games = require('./games')
 const rates = require('./rates')
-
-router.use('/users', users)
 
 router.use('/games', games)
 
