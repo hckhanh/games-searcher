cf7c84fe124461e6fbd63e99fd090f5b29514ea4
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-07-01 13:40:39 +0700

    Force SSL on connection

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const opbeat = require('opbeat')
 
 const APP_URL = process.env.APP_URL
 
+// If an incoming request uses a protocol other than HTTPS,
+// redirect that request to the same url but with HTTPS
+const forceSSL = function() {
+  return function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      )
+    }
+    next()
+  }
+}
+
 const index = require('./routes/index')
 
 const app = express()
@@ -15,6 +28,11 @@ const app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+// Force the connection need to run on SSL
+if (process.env.NODE_ENV === 'production') {
+  app.use(forceSSL())
+}
 
 app.use(compression())
 app.use(favicon(path.resolve(__dirname, 'client', 'styles', 'images', 'logo-icon.png')))
@@ -26,7 +44,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', APP_URL)
   next()
 })
@@ -34,7 +52,7 @@ app.use(function (req, res, next) {
 app.use('/', index)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
@@ -42,7 +60,7 @@ app.use(function (req, res, next) {
 
 // error handler
 //noinspection JSUnusedLocalSymbols
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   const env = req.app.get('env')
 
   // send error to Opbeat
