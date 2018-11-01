05e8a7cfac0a87298dcac0be617cf643be991b43
Khanh Hoang <hoangchaukhanh93@gmail.com>
2018-11-01 13:47:43 +0700

    Filter AAA games

--- apis/steam.js
+++ apis/steam.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOP_GAMES: 'http://www.cheapshark.com/api/1.0/deals?storeID=1',
+  TOP_GAMES: 'http://www.cheapshark.com/api/1.0/deals?storeID=1&AAA=1',
   GAME_DETAIL: 'http://store.steampowered.com/api/appdetails?appids=',
   SEARCH_GAME_BY_NAME: 'http://store.steampowered.com/api/storesearch?term='
 }
